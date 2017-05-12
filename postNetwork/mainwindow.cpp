013c50b5ebb8422f41315c628d49d1c4efb65cf1
fairytalefu <fairytalefu@gmail.com>
2017-05-12 08:43:44 +0800

    add postNetwork module

---
+++ postNetwork/mainwindow.cpp
@@ -0,0 +1,44 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QNetworkReply>
+#include <QNetworkRequest>
+#include <QtNetwork>
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+  //  QUrl myURL(QString("https://things.ubidots.com/api/v1.6/variables/5901ade07625427cad17e91b/values/?token=wseZ3mpbc7vPzM4tlANdLo5Tqngf48"));
+    QUrl myURL(QString("http://127.0.0.1:8888/BaiduMap"));
+    QNetworkRequest request(myURL);
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+    QNetworkAccessManager mgr;
+    QJsonObject json;
+    json.insert("PVStationID", 1);
+    json.insert("Voltage", 34);
+    json.insert("Current", 34);
+    json.insert("Power", 34);
+    json.insert("Vmp", 34);
+    json.insert("Imp", 34);
+    json.insert("Temp", 14);
+    json.insert("Irr", 13);
+    json.insert("Status", "open1");
+    json.insert("Lat", 34);
+    json.insert("Lng", 34);
+    json.insert("Time", "34");
+    QNetworkReply *reply = mgr.post(request, QJsonDocument(json).toJson());
+
+    while(!reply->isFinished())
+    {
+           qApp->processEvents();
+    }
+    QByteArray response_data = reply->readAll();
+    QJsonDocument response_json = QJsonDocument::fromJson(response_data);
+    reply->deleteLater();
+    qDebug() << "Success" << response_data << endl;
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
---
+++ postNetwork/postNetwork.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2017-04-27T15:36:26
+#
+#-------------------------------------------------
+
+QT       += core gui
+QT += network
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = postNetwork
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
